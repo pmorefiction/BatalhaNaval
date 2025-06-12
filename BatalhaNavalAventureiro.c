fdbed4cda4831270faf471d13be219da3d4a88c4
Samara Chaves <samarachaves1999@hotmail.com>
2025-06-12 18:37:56 -0300

    Update BatalhaNavalAventureiro.c
    Posicionamento horizontal e vertical dos navios (diferente do nível passado, coloquei os números na coluna e as letras na linha)

--- BatalhaNavalAventureiro.c
+++ BatalhaNavalAventureiro.c
@@ -11,3 +11,31 @@ int main() {
             tabuleiro[i][j] = 0;
         }
     }
+    //Horizontal
+    int linhaH = 1;
+    int colunaH = 2;
+    int sobreposicao = 0;
+    for (int j = 0; j < NAVIO; j++) {
+        if (tabuleiro[linhaH][colunaH + j] != 0) {
+            sobreposicao = 1;
+        }
+    }
+    if (!sobreposicao && colunaH + NAVIO <= TAM) {
+        for (int j = 0; j < NAVIO; j++) {
+            tabuleiro[linhaH][colunaH + j] = 3;
+        }
+    }
+    //Vertical
+    int linhaV = 4;
+    int colunaV = 6;
+    sobreposicao = 0;
+    for (int i = 0; i < NAVIO; i++) {
+        if (tabuleiro[linhaV + i][colunaV] != 0) {
+            sobreposicao = 1;
+        }
+    }
+    if (!sobreposicao && linhaV + NAVIO <= TAM) {
+        for (int i = 0; i < NAVIO; i++) {
+            tabuleiro[linhaV + i][colunaV] = 3;
+        }
+    }
